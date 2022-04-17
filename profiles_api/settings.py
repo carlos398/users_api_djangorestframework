fc7730fa3dac2a748d4ad33b5b154c3fb763643d
Carlos Andres Reyes Peña <carlosarp2000@gmail.com>
2022-04-17 16:59:15 -0500

    users auth personalizado y configurado en settings

--- profiles_api/settings.py
+++ profiles_api/settings.py
@@ -29,6 +29,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'rest_framework.authtoken',
+    
+    'profiles',
 ]
 
 MIDDLEWARE = [
@@ -113,3 +118,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'profiles.UserProfile'
