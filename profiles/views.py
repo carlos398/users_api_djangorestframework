58bcc82181d3aa96fc75d582e4d6f84cb1c6b5f0
Carlos Andres Reyes Peña <carlosarp2000@gmail.com>
2022-04-21 19:23:27 -0500

    iniciamos serializadors

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Api view de prueba"""
+    
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        
+        an_apiview = [
+            'usamos metodos HTTP como funciones (get,post,patch,put,delete)',
+            'es similar a una vista tradicional de django',
+            'nos da mayor control sobre la loginca de nuestra aplicacion',
+            'esta mapeado manualmente a los urls'
+        ]
+        
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
